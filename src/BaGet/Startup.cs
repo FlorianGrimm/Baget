099350d883d89d36b6bf4a0c300d7d3107039e47
FlorianGrimm <grimmuko@googlemail.com>
2021-02-07 04:30:19 +0100

    cop https://github.com/loic-sharma/BaGet

---
+++ src/BaGet/Startup.cs
@@ -0,0 +1,123 @@
+using System;
+using BaGet.Core;
+using BaGet.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.AspNetCore.SpaServices.StaticFiles;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace BaGet
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBaGetOptions<IISServerOptions>(nameof(IISServerOptions));
+
+            // TODO: Ideally we'd use:
+            //
+            //       services.ConfigureOptions<ConfigureBaGetOptions>();
+            //
+            //       However, "ConfigureOptions" doesn't register validations as expected.
+            //       We'll instead register all these configurations manually.
+            // See: https://github.com/dotnet/runtime/issues/38491
+            services.AddTransient<IConfigureOptions<CorsOptions>, ConfigureBaGetOptions>();
+            services.AddTransient<IConfigureOptions<FormOptions>, ConfigureBaGetOptions>();
+            services.AddTransient<IConfigureOptions<ForwardedHeadersOptions>, ConfigureBaGetOptions>();
+            services.AddTransient<IConfigureOptions<IISServerOptions>, ConfigureBaGetOptions>();
+            services.AddTransient<IValidateOptions<BaGetOptions>, ConfigureBaGetOptions>();
+
+            services.AddSpaStaticFiles(ConfigureSpaStaticFiles);
+            services.AddBaGetWebApplication(ConfigureBaGetApplication);
+
+            // You can swap between implementations of subsystems like storage and search using BaGet's configuration.
+            // Each subsystem's implementation has a provider that reads the configuration to determine if it should be
+            // activated. BaGet will run through all its providers until it finds one that is active.
+            services.AddScoped(DependencyInjectionExtensions.GetServiceFromProviders<IContext>);
+            services.AddTransient(DependencyInjectionExtensions.GetServiceFromProviders<IStorageService>);
+            services.AddTransient(DependencyInjectionExtensions.GetServiceFromProviders<IPackageService>);
+            services.AddTransient(DependencyInjectionExtensions.GetServiceFromProviders<ISearchService>);
+            services.AddTransient(DependencyInjectionExtensions.GetServiceFromProviders<ISearchIndexer>);
+
+            services.AddCors();
+        }
+
+        private void ConfigureSpaStaticFiles(SpaStaticFilesOptions options)
+        {
+            // In production, the UI files will be served from this directory
+            options.RootPath = "BaGet.UI/build";
+        }
+
+        private void ConfigureBaGetApplication(BaGetApplication app)
+        {
+            // Add database providers.
+            app.AddAzureTableDatabase();
+            //app.AddMySqlDatabase();
+            //app.AddPostgreSqlDatabase();
+            //app.AddSqliteDatabase();
+            //app.AddSqlServerDatabase();
+
+            // Add storage providers.
+            app.AddFileStorage();
+            //app.AddAliyunOssStorage();
+            //app.AddAwsS3Storage();
+            app.AddAzureBlobStorage();
+            app.AddGoogleCloudStorage();
+
+            // Add search providers.
+            app.AddAzureSearch();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            var options = Configuration.Get<BaGetOptions>();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+
+            app.UseForwardedHeaders();
+            app.UsePathBase(options.PathBase);
+
+            app.UseSpaStaticFiles();
+
+            app.UseRouting();
+
+            app.UseCors(ConfigureBaGetOptions.CorsPolicy);
+            app.UseOperationCancelledMiddleware();
+
+            app.UseEndpoints(endpoints =>
+            {
+                var api = new BaGetApi();
+
+                api.MapRoutes(endpoints);
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "../BaGet.UI";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
